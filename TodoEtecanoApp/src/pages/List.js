b3458b9d1c4f3b7ac9a1f3ec2cf8ac774b41ff6f
Jhonnatthan <jhonnatthans@outlook.com>
2019-11-27 22:31:32 -0300

    [ADD] - Template do APP

--- TodoEtecanoApp/src/pages/List.js
+++ TodoEtecanoApp/src/pages/List.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 
 export default class List extends Component {
+    static navigationOptions = {
+        title: "Lista de TODO's"
+    }
+
     render = () => {
         return <View />;
     }
